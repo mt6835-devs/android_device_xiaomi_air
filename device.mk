18c844295f56cdb9b8976549bffbe2debf89cf4c
Benedict Wong <benedictwong@google.com>
2019-07-16 12:57:20 -0700

    air: Add feature flag for FEATURE_IPSEC_TUNNELS
    This commit adds the IPSEC_TUNNELS feature flag, with XFRM Interfaces
    used as the implementation of choice.
    
    Bug: 117183273
    Bug: 137629222
    Test: Compiles, passes tunnel-mode CTS tests
    Test: adb shell pm list features
    Change-Id: I19c598947525783850b29d149db85db89f8b347e

--- device.mk
+++ device.mk
@@ -188,7 +188,8 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # Sensors
 PRODUCT_PACKAGES += \
