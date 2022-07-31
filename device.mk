835b0d5ff5474eab6bd6c7281b5b59f61b29ae56
wbs306 <wbs306@gmail.com>
2022-07-31 12:00:05 +0800

    air: Add symlink for mdota
    Change-Id: I93cf8ee9a504c801e0e238be06ba4cc17387e62c

--- device.mk
+++ device.mk
@@ -187,6 +187,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
 # Radio
+PRODUCT_PACKAGES += \
+    vendor_mdota_symlink
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
