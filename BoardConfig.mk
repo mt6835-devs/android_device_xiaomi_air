cf91666adf9b64544a97d1bb1b24debd79bb8e67
guibing <guibing@google.com>
2023-10-25 17:26:08 +0000

    air: Set PELT HALFLIFE to 8ms before early init boot stage
    It's for early boot stage boost. After early init stage, change
    it back to the default 32ms.
    
    This is from the similar change we had in zuma: ag/23681609
    
    Bug: 300516320
    Test: check the device boot.
    Change-Id: I2286a94c31480766540e20cab619cc87363bdd1f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 BOARD_KERNEL_CMDLINE += bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem
+BOARD_KERNEL_CMDLINE +=  sysctl.kernel.sched_pelt_multiplier=4
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_DTB_OFFSET := 0x47c80000
