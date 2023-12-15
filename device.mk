aae70be03a044015b4a11ae9309ecee184efc3c2
bengris32 <bengris32@protonmail.ch>
2023-12-15 21:07:23 +0000

    air: Add shim for MediaTek AudioTrack constructor
    Change-Id: Icc7a1f9c748578f5b58499e7a76213673c4bcf54
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -196,6 +196,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
+PRODUCT_COPY_FILES += \
+    libaudioclient_shim
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
