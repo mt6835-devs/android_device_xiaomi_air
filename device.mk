acf2d5b589fcbf481b857af15795d4cce66dd318
Woomymy <woomy@woomy.be>
2024-10-29 17:10:36 +0100

    air: Inherit `generic_ramdisk` product
    Change-Id: If3254c3d3c076532f3bd83cf239beb69f1fd54c6
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: Alvyrion <alvyrion0@gmail.com>

--- device.mk
+++ device.mk
@@ -3,8 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/xiaomi/air
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
+DEVICE_PATH := device/xiaomi/air
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
