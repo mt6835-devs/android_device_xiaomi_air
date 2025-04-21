c9f4fa3c2adda9a668cee5001606b39e6457b57c
claxten10 <claxten10@gmail.com>
2025-04-22 00:43:04 +0200

    air: Configure Gatekeeper stack
    Change-Id: I250d5b4c84fbd9b4dda9154d20561fe272ae7626
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -99,6 +99,11 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot-service.example_recovery \
     fastbootd
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
