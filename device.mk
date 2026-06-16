20a0cf4e184bcfccf5b8032bbf60bd13b1131ac9
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-06-16 22:58:10 +0700

    fixup! air: Configure Radio stack

--- device.mk
+++ device.mk
@@ -239,9 +239,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
-PRODUCT_COPY_FILES += \
-    libaudioclient_shim
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
