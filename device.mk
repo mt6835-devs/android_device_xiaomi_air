4947cd1fa3b0d8777f11c5da597b844b8fc8c712
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-06-16 21:32:18 +0700

    air: Copy Verified Boot permission

--- device.mk
+++ device.mk
@@ -300,6 +300,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.mediatek
 
+# Verified Boot
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
