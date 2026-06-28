2e0a79223c127d58b3b9fe507db541236ad7c1ce
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-06-28 02:40:07 +0000

    air: sepolicy: Address more denials
    Change-Id: I3a251339b4c5c71b8615be5bb420f73fdcc46f59

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,8 @@ VENDOR_SECURITY_PATCH := 2026-03-05
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
